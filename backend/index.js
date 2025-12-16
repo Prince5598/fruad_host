c880f26660ad19a8b60683fb8eba40178da904b0
prince <princepatel5598@gmail.com>
2025-12-16 14:53:51 +0530

    changing index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const adminRoutes = require('./routes/adminRoute');
 
 require('dotenv').config();
 db.connect();
-app.use(cors({ origin: 'https://fruad-host-a24ng74u9-princepatel5598-7326s-projects.vercel.app', credentials: true,exposedHeaders: ['Authorization'], }));
+app.use(cors({ origin: 'https://fruad-host-692mra9tl-princepatel5598-7326s-projects.vercel.app', credentials: true,exposedHeaders: ['Authorization'], }));
 
 app.use(express.json());
 app.use(cookieParser());
